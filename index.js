c1433f59861ec8bacc6ddff89c6bb316bc96dec1
jeet <jeet22761@gmail.com>
2024-07-07 14:08:14 +0530

    upload to main

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname, "public")));
 const port = 3000;
 app.listen(port,()=>{
   console.log("app ");
 })
 
 app.get('/',(req,res)=>{
-  res.send("hello");
+res.render("front.ejs");
 })
